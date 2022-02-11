e06d9cc4a4aac0783b18492eb2e144454f5c9506
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-11 12:43:08 +0200

    Add Tests for Home component

--- src/components/Apis.js
+++ src/components/Apis.js
@@ -2,7 +2,6 @@ const key = 'bbce26616140cb78fbac5081d8a07a35';
 const Apis = {
   profiles: 'https://financialmodelingprep.com/api/v3/profile/',
   stocksList: `https://financialmodelingprep.com/api/v3/stock/list?apikey=${key}`,
-  search: 'https://financialmodelingprep.com/api/v3/search?query=',
 };
 
 export default Apis;
